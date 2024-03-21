d663f23f224d1c0ce805c167ee2ed7d006afcafd
Joaquin Valois Ruiz <Joaco0017@hotmail.com>
2024-03-21 12:06:56 -0600

    Añadiendo 4 funciones cientificas a la calculadora

--- proyects/P7_2/index.js
+++ proyects/P7_2/index.js
@@ -1,4 +1,5 @@
 let resultado = document.getElementById('resultado');
+let input = document.getElementById('resultado');
 
 function asignar(valor){
     resultado.value += valor;
@@ -6,7 +7,11 @@ function asignar(valor){
 }
 
 function calcular(){
+    if (input.value === '') {
+        alert('Ingrese un número válido');
+    } else {
     resultado.value = eval(resultado.value);
+    }
 }
 
 //Asigna la función a una variable
@@ -14,6 +19,55 @@ function calcular(){
     resultado.value = eval(resultado.value);
 }*/
 
+function raizCuadrada() {
+    let valor = parseFloat(resultado.value);
+    if (input.value === '') {
+        alert('Ingrese un número válido');
+    } else {
+        resultado.value = Math.sqrt(valor);
+    }
+}
+
+function sen() {
+    let valor = parseFloat(resultado.value);
+    if (input.value === '') {
+        alert('Ingrese un número válido');
+    } else {
+        resultado.value = Math.sin(valor);
+    }
+}
+
+function cos() {
+    let valor = parseFloat(resultado.value);
+    if (input.value === '') {
+        alert('Ingrese un número válido');
+    } else {
+        resultado.value = Math.cos(valor);
+    }
+}
+
+function logaNat(){
+    let valor = parseFloat(resultado.value);
+    if (input.value === '') {
+        alert('Ingrese un número válido');
+    } else {
+        resultado.value = Math.log(valor);
+    }
+}
+
+function porcentaje(){
+    let valor = parseFloat(resultado.value);
+    if (input.value === '') {
+        alert('Ingrese un número válido');
+    } else {
+        resultado.value = Math.pow(valor(50)) / 100;
+    }
+}
+
 function limpiar(){
-    resultado.value = '';
+    if (input.value === '') {
+        alert('No hay nada que borrar');
+    }else{
+        resultado.value = '';
+    }
 }
