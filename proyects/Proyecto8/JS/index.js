3005e0cb6802c5e413b0549b233ae320d9da5ba2
Joaco_Vrz <Joaco0017@hotmail.com>
2024-04-23 23:35:28 -0600

    añadiendo registro de interfaz de las tareas ingresadas

--- proyects/Proyecto8/JS/index.js
+++ proyects/Proyecto8/JS/index.js
@@ -4,17 +4,24 @@ let fecha = document.getElementById('fecha');
 let descripcion = document.getElementById('descripcion');
 
 let tareas = [];
+let listaTareas = document.getElementById('listaTareas');
 let btnGuardar = document.getElementById('btnGuardar');
+
+function mostrarTareas(){
+    listaTareas.innerHTML = 'Lista de tareas: ';
+    tareas.forEach((tarea,indice)=>{listaTareas.innerHTML += `<p>${tarea.nombre}<p>`});
+}
 
 let agregarDatos = ()=>{
     alert('Datos guardados con éxito');
-    tareas.push({
+    let datos = {
         nombre: nombre.value,
         fecha: fecha.value,
         descripcion: descripcion.value
-    });
-    tareas.push(tareas);
+    };
+    tareas.push(datos);
     console.log(tareas);
+    mostrarTareas();
 }
 
 formulario.addEventListener('submit', function(e){
