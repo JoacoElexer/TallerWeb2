983ec170bff4e5fdfc3d40ff63f9acce2024ba79
Joaco_Vrz <Joaco0017@hotmail.com>
2024-06-09 14:46:43 -0600

    Version corregida de la app tienda

--- proyects/P10-2/js/categorias.js
+++ proyects/P10-2/js/categorias.js
@@ -1,19 +1,17 @@
-let listaCategorias = document.querySelector('#listaCategorias');
+let listaCategorias = document.querySelector("#listaCategorias");
 let obtieneCategorias = () => {
     fetch('https://fakestoreapi.com/products/categories')
         .then(res => res.json())
         .then(categorias => {
-            //console.log(categorias);
-            listaCategorias.innerHTML = '';
+            listaCategorias.innerHTML = "";
             categorias.forEach((categoria, indice) => {
                 categoria = categoria.replace("'", '');
-                //console.log("Categoria: ", indice, categoria);
                 listaCategorias.innerHTML += `
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" 
+                        <a class="nav-link" aria-current="page"
                         onclick="muestraProductos('${categoria}');"
                         href="#">${categoria.toUpperCase()}</a>
-                    </li>`;
+                    </li> `;
             });
-        });
-}
+        })
+};
---
+++ proyects/P10-2/js/index.js
@@ -0,0 +1,4 @@
+let btnBuscar = document.querySelector("#btnBuscar");
+let textoBuscar = document.querySelector("#textoBuscar");
+obtieneProductos();
+obtieneCategorias();
